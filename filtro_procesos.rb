56351c1cc0a3857437a55d728d252d4134945c23
MirsonCamadro <kztempus@gmail.com>
2020-07-11 08:56:49 -0400

    final update

--- filtro_procesos.rb
+++ filtro_procesos.rb
@@ -1,8 +1,6 @@
 data = File.open('procesos.data').readlines.map(&:chomp)
 
-
 filter_number = ARGV[0].to_i
-
 output = data.select{|x| x.to_i > filter_number }
 
 File.write('procesos_filtrados.data', output )
