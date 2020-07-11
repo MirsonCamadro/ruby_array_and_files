1dd8b4d3338e41b457537f7953dfde2bae328779
MirsonCamadro <kztempus@gmail.com>
2020-07-11 08:55:23 -0400

    update visitas.rb

--- proyecciones.rb
+++ proyecciones.rb
@@ -3,9 +3,7 @@ data = File.open("ventas_base.db").read.split(",").map!{|x| x.to_i}
 def metodo(arr, aumento, desde, hasta)
     rango = arr[desde..hasta]
     result = rango.inject{|sum,n| sum.to_i + n.to_i } * aumento.to_f
-    print result
-    print '---'
 end
 
-metodo(data, 1.1, 0, 5)
-metodo(data, 1.2, 6, 11)
+pp metodo(data, 1.1, 0, 5)
+pp metodo(data, 1.2, 6, 11)
--- visitas.rb
+++ visitas.rb
@@ -1,10 +1,16 @@
 visitas = [1000, 800, 250, 300, 500, 2500]
 
+# def promedio(visitors)
+#     days = visitors.count
+#     visits_total = visitors.inject{ |sum, n| sum + n }
+#     a = (visits_total.to_f/days)
+#     print a
+# end
+
+# promedio(visitas)
+
 def promedio(visitors)
-    days = visitors.count
-    visits_total = visitors.inject{ |sum, n| sum + n }
-    a = (visits_total.to_f/days)
-    print a
+    visitors.sum.to_f/visitors.count
 end
 
-promedio(visitas)
+pp promedio(visitas)
