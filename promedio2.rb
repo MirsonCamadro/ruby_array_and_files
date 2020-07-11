727485ed64ac3532aa79c9f4e8439c807d87b9aa
MirsonCamadro <kztempus@gmail.com>
2020-07-11 08:50:07 -0400

    update prommedio2.rb

--- promedio2.rb
+++ promedio2.rb
@@ -3,13 +3,8 @@ b = [10, 20, 30]
 
 def compara_arrays(arr1, arr2)
     avrg_arr = []
-
-    arr1_avrg = arr1.inject{|sum, n| sum + n }.to_f / arr1.count
-    avrg_arr.push(arr1_avrg) 
-    arr2_avrg = arr2.inject{|sum, n| sum + n }.to_f / arr1.count
-    avrg_arr.push(arr2_avrg) 
-    print avrg_arr
-
+    avrg_arr.push(arr1.inject{|sum, n| sum + n }.to_f / arr1.count) 
+    avrg_arr.push(arr2.inject{|sum, n| sum + n }.to_f / arr1.count) 
 end
 
-compara_arrays(a,b)
+pp compara_arrays(a,b)
